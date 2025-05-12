4820479b2627c5460f39af867f4e6c1ee148df04
Ehan <ehanahamed@ehan.dev>
2025-05-12 17:48:56 -0400

    Setup Spring

---
+++ src/test/java/org/quizfreely/classes/ClassesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.quizfreely.classes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
