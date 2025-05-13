2dd21bb9625e5538eb297c3849815d665a8618ef
Ehan <ehanahamed@ehan.dev>
2025-05-13 19:32:52 -0400

    Finish implementing ClassEntity.java

--- src/main/java/org/quizfreely/classes/ClassController.java
+++ src/main/java/org/quizfreely/classes/ClassController.java
@@ -7,12 +7,18 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ClassController {
+    @Autowired
+    private ClassRepo classRepo;
+    private CourseRepo courseRepo;
+
     @QueryMapping
     public ClassEntity classById(@Argument long id) {
         return classRepo.findById(id);
     }
 
     @SchemaMapping
-    public 
+    public Course course(ClassEntity classEntity) {
+        return CourseRepo.getById(classEntity.courseId)
+    }
 }
 
--- src/main/java/org/quizfreely/classes/ClassEntity.java
+++ src/main/java/org/quizfreely/classes/ClassEntity.java
@@ -16,5 +16,38 @@ public class ClassEntity {
     private List<Teacher> teachers;
     private List<Student> students;
     private Course course;
+
+    public ClassEntity() {
+        name = "Untitled Class";
+    }
+    public ClassEntity(long id) {
+        name = "Untitled Class";
+    }
+    public ClassEntity(long id, String name) {
+        this.name = name;
+    }
+    public ClassEntity(long id, String name, Course course) {
+        this.name = name;
+        this.course = course;
+    }
+    public ClassEntity(
+        long id, String name, Course course, List<Teacher> teachers
+    ) {
+        this.name = name;
+        this.course = course;
+        this.teachers = teachers;
+    }
+    public ClassEntity(
+        long id,
+        String name,
+        Course course,
+        List<Teacher> teachers,
+        List<Student> students
+    ) {
+        this.name = name;
+        this.course = course;
+        this.teachers = teachers;
+        this.students = students;
+    }
 }
 
