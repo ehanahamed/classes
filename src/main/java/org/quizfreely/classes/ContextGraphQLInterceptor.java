9a231a0968f5afd3ddc75d5d912c53b3a5aff64b
Ehan <ehanahamed@ehan.dev>
2025-05-15 23:04:21 -0400

    Fix error w missing pkgdef, strlen & urlenc

--- src/main/java/org/quizfreely/classes/ContextGraphQLInterceptor.java
+++ src/main/java/org/quizfreely/classes/ContextGraphQLInterceptor.java
@@ -1,3 +1,5 @@
+package org.quizfreely.classes;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.server.WebGraphQlInterceptor;
 import org.springframework.graphql.server.WebGraphQlRequest;
@@ -5,6 +7,8 @@ import org.springframework.graphql.server.WebGraphQlResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import org.springframework.http.HttpCookie;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import org.quizfreely.classes.auth.AuthRepo;
 
 @Component
@@ -20,6 +24,7 @@ public class ContextGraphQLInterceptor implements WebGraphQlInterceptor {
         String authHeader = request.getHeaders().getFirst("Authorization");
         if (
             authHeader != null &&
+            authHeader.length() > 7 &&
             authHeader.substring(
                 0, 7 /* "bearer " is first 7 characters */
             ).toLowerCase().equals("bearer ")
@@ -29,7 +34,10 @@ public class ContextGraphQLInterceptor implements WebGraphQlInterceptor {
             /* try getting "auth" cookie */
             HttpCookie authCookie = request.getCookies().getFirst("auth");
             if (authCookie != null) {
-                authToken = authCookie.getValue();
+                authToken = URLDecoder.decode(
+                    authCookie.getValue(),
+                    StandardCharsets.UTF_8
+                );
             }
         }
 
